2423ddec139d72e6172bec3997709123c44d5e85
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-05-05 13:22:22 +1000

    add permissions and user info

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ import bulletin_config
 app = flask.Flask(__name__)
 
 
+def get_permissions(user_id):
+    return ["admin"]
+
+
 @app.route('/')
 def index():
-    return flask.render_template('student-index.html.j2', bulletin_config=bulletin_config)
+    user_info = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john.doe@example.com",
+        "name": "John Doe",
+    }
+    return flask.render_template(
+        'index.html.j2',
+        bulletin_config=bulletin_config,
+        permissions=get_permissions(1),
+        user_info=user_info
+    )
