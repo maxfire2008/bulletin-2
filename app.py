71eb6b84f40b62c91169a41c27a2053d1d30896f
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-05-06 18:54:11 +1000

    add database

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 # flask app
 import flask
 import bulletin_config
+import psycopg2
+
+conn_string = "host=" + "127.0.0.1" + " port=" + "5432" + " dbname=" + "bulletin-2" + " user=" + "bulletin-2" \
+    + " password=" + input("Enter postgres password: ")
+conn = psycopg2.connect(conn_string)
 
 app = flask.Flask(__name__)
 
+
+def database_connection
+
+#SELECT distinct("date") FROM "bulletin-2".occurrences ORDER BY "date" DESC LIMIT 2;
 
 def get_permissions(user_id):
     return ["admin"]
--- example_bulletin_config.py
+++ example_bulletin_config.py
@@ -1,3 +1,9 @@
 SCHOOL_NAME = "Sunny Valley High School"
 SCHOOL_NAME_SHORT = "SVHS"
 APP_NAME = "Bulletin Viewer"
+
+DATABASE_HOST=127.0.0.1
+DATABASE_PORT=5432
+DATABASE_NAME="bulletin-2"
+DATABASE_USER="bulletin-2"
+DATABASE_PASSWORD="password"
