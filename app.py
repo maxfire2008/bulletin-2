bdb9c5290112c46899774e2840fadd57de8116d8
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-05-07 18:43:03 +1000

    add custom error handlers

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ SAMPLE_USER_INFO = {
     "last_name": "Citizen",
     "email": "john.citizen@example.com",
     "name": "John Citizen",
+    "id": 1,
 }
 
 # MARKDOWN_IT_RENDERER = (
@@ -60,11 +61,13 @@ def fetch_bulletins(count: int = 50, offset: int = 0, earlier_than: datetime.dat
     conn.close()
     return results
 
+
 def grades_string(grades: list):
     if -1 in grades:
         return "<i>All Grades</i>"
     else:
-        return "<b>"+"</b> & <b>".join(map(str, grades))+"</b>"
+        return "<b>"+"</b>, <b>".join(map(str, grades))+"</b>"
+
 
 def fetch_items_for_day(date: datetime.date):
     conn = database_connection()
@@ -109,38 +112,100 @@ def get_age_from_time(time_from: datetime.datetime):
 
 
 def get_permissions(user_id):
-    return ["admin"]
+    return ["admin", "superuser"]
 
 
-@ app.route('/')
+@app.route('/')
 def index():
     return flask.render_template(
         'index.html.j2',
         current_page="/",
         PAGE_NAMES=PAGE_NAMES,
         bulletin_config=bulletin_config,
-        permissions=get_permissions(1),
+        permissions=get_permissions(SAMPLE_USER_INFO["id"]),
         user_info=SAMPLE_USER_INFO,
         bulletins=fetch_bulletins(earlier_than=datetime.date.today()),
         get_age_from_time=get_age_from_time
     )
 
 
-@ app.route("/bulletin/")
+@app.route("/bulletin/")
 def bulletin():
     # get the date from ?date
     date = flask.request.args.get("date")
     if date:
-        date = datetime.date.fromisoformat(date)
+        try:
+            date = datetime.date.fromisoformat(date)
+        except:
+            return "400: Invalid date", 400
+        if (datetime.date.today()-date).days >= 0 or "view_early" in get_permissions(SAMPLE_USER_INFO["id"]):
+            return flask.render_template(
+                'bulletin.html.j2',
+                current_page="/bulletin/",
+                PAGE_NAMES=PAGE_NAMES,
+                bulletin_config=bulletin_config,
+                permissions=get_permissions(SAMPLE_USER_INFO["id"]),
+                user_info=SAMPLE_USER_INFO,
+                bulletin_date=date,
+                base64_encode=base64.b64encode,
+                viewing_early=(not (datetime.date.today()-date).days >= 0)
+            )
+        else:
+            return flask.render_template(
+                "error.html.j2",
+                error="403: You are not allowed to view the bulletin early",
+                current_page="error",
+                PAGE_NAMES=PAGE_NAMES,
+                bulletin_config=bulletin_config,
+                permissions=get_permissions(SAMPLE_USER_INFO["id"]),
+                user_info=SAMPLE_USER_INFO,
+            ), 403
+    else:
         return flask.render_template(
-            'bulletin.html.j2',
-            current_page="/bulletin/",
+            "error.html.j2",
+            error="400: This request does not have a date",
+            current_page="error",
             PAGE_NAMES=PAGE_NAMES,
             bulletin_config=bulletin_config,
-            permissions=get_permissions(1),
+            permissions=get_permissions(SAMPLE_USER_INFO["id"]),
             user_info=SAMPLE_USER_INFO,
-            bulletin_items=fetch_items_for_day(date),
-            bulletin_date=date,
-            base64_encode=base64.b64encode,
-            # MARKDOWN_IT_RENDERER=MARKDOWN_IT_RENDERER
-        )
+        ), 400
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return flask.render_template(
+        "error.html.j2",
+        error="404: Page not found",
+        current_page="error",
+        PAGE_NAMES=PAGE_NAMES,
+        bulletin_config=bulletin_config,
+        permissions=get_permissions(SAMPLE_USER_INFO["id"]),
+        user_info=SAMPLE_USER_INFO,
+    ), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return flask.render_template(
+        "error.html.j2",
+        error="500: Server error",
+        current_page="error",
+        PAGE_NAMES=PAGE_NAMES,
+        bulletin_config=bulletin_config,
+        permissions=get_permissions(SAMPLE_USER_INFO["id"]),
+        user_info=SAMPLE_USER_INFO,
+    ), 400
+
+
+@app.route("/coffee/")
+def coffee():
+    return flask.render_template(
+        "error.html.j2",
+        error="I don't really like coffee even though I'm a programmer.",
+        current_page="error",
+        PAGE_NAMES=PAGE_NAMES,
+        bulletin_config=bulletin_config,
+        permissions=get_permissions(SAMPLE_USER_INFO["id"]),
+        user_info=SAMPLE_USER_INFO,
+    ), 418
