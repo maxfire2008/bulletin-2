4d5354e6fac07438b76b80426d4b1bd56e4f6d83
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-06-06 11:59:56 +1000

    changed default config

--- example_bulletin_config.py
+++ example_bulletin_config.py
@@ -2,7 +2,15 @@ SCHOOL_NAME = "Sunny Valley High School"
 SCHOOL_NAME_SHORT = "SVHS"
 APP_NAME = "Bulletin Viewer"
 
-DEFAULT_VISIBILITIES = ["public", "student", "grade1", "grade2", "grade3", "grade4", "staff"]
+DEFAULT_VISIBILITIES = [
+    "user_type:public",
+    "user_type:student",
+    "user_type:staff",
+    "grade:1",
+    "grade:2",
+    "grade:3",
+    "grade:4",
+    ]
 
 DATABASE_HOST="127.0.0.1"
 DATABASE_PORT=5432
